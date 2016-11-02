94730a7b0b8aaae66886ec8f602a111eedb2478a
Martijn Posthuma <martijn13795@hotmail.com>
2016-11-02 23:02:57 +0100

    version update

--- ScoreTest/Form1.cs
+++ ScoreTest/Form1.cs
@@ -120,8 +120,6 @@ namespace ScoreBord
                                 }
                                 form2.Show();
                                 form3.Show();
-                                form2.TopMost = false;
-                                form3.TopMost = false;
                                 m = Convert.ToInt32(numericUpDown1.Value);
                                 s = 0;
                                 string mm = Convert.ToString(m);
